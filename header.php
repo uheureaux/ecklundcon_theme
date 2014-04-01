2595a3f66c02c053ad19127bef768f80397096e3
RaffBluffin <RafffCollado@gmail.com>
2014-04-01 17:50:21 -0400

    Modify header.php
    Fixed title.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title><?php bloginfo('title')?>')?></title>
+        <title><?php bloginfo('title')?>)?></title>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
         <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
         <?php wp_head()?>
