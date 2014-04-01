9ec2b1723cb54a81b7f8902b57d095dcfd617e95
RaffBluffin <RafffCollado@gmail.com>
2014-04-01 17:10:18 -0400

    Created functios.php
    Created functions.php as well as a slight modification to header.php.

--- header.php
+++ header.php
@@ -4,13 +4,14 @@
         <meta charset="utf-8">
         <title><?php bloginfo('title')?>')?></title>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" />
+        <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
         <?php wp_head()?>
     </head>
         
     <body>
         <div id="container">
             <header>
-                <h1><?php bloginfo('name')?></h1>
+                <h1><a href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a></h1>
                 <nav>
                     <?php wp_nav_menu()?>
                 </nav>
