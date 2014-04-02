eb911fefd67b0c57fbbc0b75cc8da2583017c7bb
RaffBluffin <RafffCollado@gmail.com>
2014-04-02 16:41:42 -0400

    Modify index.php and style.css
    Added div to surround each post.
    Added css for post id.

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <div class="left">
         <div class="posts">
             <!-- START POST LOOP -->
-            <?php while(have_posts()): the_post() ?>     
-            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2> by <?php the_author() ?> at <?php the_time() ?>
+            <?php while(have_posts()): the_post() ?>  
+            <div id="post">
+                <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2> by <?php the_author() ?> at <?php the_time() ?>
                 <?php the_content('Click here for more!') ?>
+            </div>
             <?php endwhile; ?>
             <!-- END LOOP -->
         </div>       
