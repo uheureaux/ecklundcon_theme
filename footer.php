a96449bae0002ec1113618c544ff4c3f18e846f2
RaffBluffin <RafffCollado@gmail.com>
2014-04-04 00:03:13 -0400

    Modify multiple files. Added multiple files.
    Added the following social media icons to footer:
    - Facebook
    - Twitter
    - Google+
    - Linkedin
    Changed the global font color to #222125 from #000.

--- footer.php
+++ footer.php
@@ -2,6 +2,10 @@
                 <div id="social_widgets">
                     <ul>
                         <!-- TODO: Add social media icons.(div not img) -->
+                        <li><a href="#"><div id="facebook-icon"></div></a></li>
+                        <li><a href="#"><div id="twitter-icon"></div></a></li>
+                        <li><a href="#"><div id="googleplus-icon"></div></a></li>
+                        <li><a href="#"><div id="linkedin-icon"></div></a></li>
                     </ul>
                 </div>
                 
--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <div id="content">   
     <div id="posts">
         <?php while(have_posts()): the_post() ?>
-            <h3><a href="<?php the_permalink()?>"><?php the_title()?></a></h3>
-            <p><?php the_content() ?></p>
+            <div id="post">
+                <h3><a href="<?php the_permalink()?>"><?php the_title()?></a></h3>
+                <p><?php the_content() ?></p>
+            </div>
         <?php endwhile; ?>
     </div>
 </div>
