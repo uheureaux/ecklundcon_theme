d6a42a7383c26fe23ef3abd9689700c482865f59
RaffBluffin <RafffCollado@gmail.com>
2014-04-03 20:42:09 -0400

    Add multiple files. Modify page.php
    Fixed page.php
    Added template files for each navigation bar page.

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
 <div id="content">
     <div id="posts">
-        <?php while(have_posts()): the_post(); ?>
+        <?php while(have_posts()): the_post() ?>
         <h3><a href="<?php the_permalink()?>"><?php the_title()?></a></h3>
         <p><?php the_content()?></p>
         <?php endwhile; ?>
