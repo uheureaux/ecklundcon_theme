3e6526efd03f5c87bdc2408b75b062f7436d20d9
uheureaux <uheureaux@gmail.com>
2014-04-04 19:57:53 -0400

    Modify single.php and style.css
    Added divs.
    Added single post section in style.css

--- single.php
+++ single.php
@@ -3,12 +3,18 @@
 <div id="content">
     <div id="single_entry">
         <?php IF(have_posts()) : while(have_posts()) : the_post(); ?>
-        <h2><?php the_title()?></h2>
-        <p><h4>by <?php the_author()?></h4>.</p>
-        <div id="post_content"><p><?php the_content()?>.</p></div>
-        <p id="post_date"><?php the_time('l F jS, Y h:i:s A') ?></p>
-        <p><a class="post_tags"><?php the_tags()?></a></p>
-        <?php previous_post_link(); next_post_link(); ?>
+        <div id="post_title">
+            <h2><?php the_title()?></h2>
+            <h4>Posted on <?php the_time('l F jS, Y')?> by <?php the_author()?></h4>
+        </div>
+        <div id="post_content">
+            <p><?php the_content()?>.</p>
+        </div>
+        <div id="post_footer">
+            <p><?php the_time('l F jS, Y') ?></p>
+            <p><?php the_tags()?></p>
+            <?php previous_post_link(); next_post_link(); ?>
+        </div>
            <?php endwhile;?>
         <?php endif;?>
     </div>
