5f472e9ff6934a68c4ea359eaef90a5f50013040
RaffBluffin <RafffCollado@gmail.com>
2014-04-03 21:08:18 -0400

    Modify multiple files. Purged multiple files.
    Added post loop to index.php for development purposes.
    Purged page.php and single.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php get_header() ?>
 
 <div id="content">   
-
+    <div id="posts">
+        <?php while(have_posts()): the_post() ?>
+            <h3><a href="<?php the_permalink()?>"><?php the_title()?></a></h3>
+            <p><?php the_content() ?></p>
+        <?php endwhile; ?>
+    </div>
 </div>
 
 <?php get_footer() ?>
